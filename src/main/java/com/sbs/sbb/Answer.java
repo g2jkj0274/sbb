592f250b7d90101bd28109eac958cbf9e6b6b011
g2jkj0274 <g2jkj0274@gmail.com>
2023-08-29 12:35:34 +0900

    점프 투 스프링 : 2-04 엔티ë티

--- src/main/java/com/sbs/sbb/Answer.java
+++ src/main/java/com/sbs/sbb/Answer.java
@@ -1,15 +1,10 @@
 package com.sbs.sbb;
 
-import java.time.LocalDateTime;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -24,5 +19,6 @@ public class Answer {
 
     private LocalDateTime createDate;
 
+    @ManyToOne
     private Question question;
 }
